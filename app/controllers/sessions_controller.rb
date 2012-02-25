11429198fbf2dae085af426adf4089e1757f3cc4
David Berner <davidajberner@gmail.com>
2012-02-25 15:40:38 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     @title = "Sign in"
   end
 
- def create
+  def create
     user = User.authenticate(params[:session][:email],
                              params[:session][:password])
     if user.nil?
@@ -12,9 +12,8 @@ class SessionsController < ApplicationController
       @title = "Sign in"
       render 'new'
     else
-      # Sign the user in and redirect to the user's show page.
-         sign_in user
-      redirect_to user
+      sign_in user
+      redirect_back_or user
     end
   end
 
